8f709938ab7d1181afe519e5fe13afa0bcd2d7fb
Ricardo Teixeira <37122343+ricardotx@users.noreply.github.com>
2019-10-25 20:15:35 +0100

    Create qbittorrent_telegram_notification.sh

---
+++ qbittorrent_telegram_notification.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Bot token
+BOT_TOKEN="ADD_YOUR_TELEGRAM_BOT_TOKEN"
+
+# Your chat id
+CHAT_ID="ADD_YOUR_TELEGRAM_CHAT_ID"
+
+# Get variables from qbittorrent
+TORRENT_NAME="$1"
+
+# Notification message
+# If you need a line break, use "%0A" instead of "\n".
+MESSAGE="<strong>Download Completed</strong>%0A${TORRENT_NAME}%0A"
+
+# Prepares the request payload
+PAYLOAD="https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${MESSAGE}&parse_mode=HTML"
+
+# Sends the notification to the telegram bot and save the response content into the notificationsLog.txt
+curl -S -X POST "${PAYLOAD}" -w "\n\n" | tee -a notificationsLog.txt
+
+# Prints a info message in the console
+echo "[${TORRENT_NAME}] Download completed. Telegram notification sent."
